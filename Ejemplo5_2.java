2c7ee2750613368db8090f9f55b79024aa393c34
Abiexperiments <abigailgarcia900200@gmail.com>
2024-11-21 16:27:37 -0600

    Add files via upload

---
+++ Ejemplo5_2.java
@@ -0,0 +1,18 @@
+package U5Ejemplos;
+import javax.swing.JOptionPane;
+public class Ejemplo5_2 {
+
+	public static void main(String[] args) {
+		//excepciones
+//capturar con joptionpane un numero
+
+		 String num = JOptionPane.showInputDialog("Ingresa un número:");
+	        
+
+	        int numero = Integer.parseInt(num);
+	        
+System.out.println(num);
+int valor=4/numero;
+	}
+
+}
